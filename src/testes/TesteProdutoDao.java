322ff83d8fb1b2bc0ed1cdbcff002a5afe0d9787
Mateus <mateuscirinosilva32@gmail.com>
2020-04-10 12:04:08 -0300

    testes

---
+++ src/testes/TesteProdutoDao.java
@@ -0,0 +1,39 @@
+package testes;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+
+import dao.ProdutoDao;
+import modelos.Produto;
+
+public class TesteProdutoDao {
+    public static void main(String[] args) {
+        Produto Produto = new Produto();
+
+        ProdutoDao ProdutoDao = new ProdutoDao();
+
+        //INSERCAO
+        /*Produto.setDescricao("Feijao");
+        Produto.setSaldo(2.5);
+        Produto.setUnidade("Kg");
+
+        ProdutoDao.inserir(Produto);*/
+
+        //UPDATE
+        /*Optional<Produto> ProdutoOpt = ProdutoDao.select(2);
+        
+        if(ProdutoOpt.isPresent()) {
+            Produto = ProdutoOpt.get();
+            Produto.setSaldo(5.5);
+            ProdutoDao.update(Produto);
+        }*/
+
+        //DELETE
+        Collection<Produto> Produtos = ProdutoDao.selectAll();
+        Produtos.forEach(
+            p -> ProdutoDao.delete(p)
+        );
+
+    }
+}
