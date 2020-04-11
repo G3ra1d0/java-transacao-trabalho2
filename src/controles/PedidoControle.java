bf40973a01f96b17deff34edf9de5f71d6ff5701
Mateus <mateuscirinosilva32@gmail.com>
2020-04-11 20:48:54 -0300

    adicao da funcao ListarPedidos e ExcluirPedidos

--- src/controles/PedidoControle.java
+++ src/controles/PedidoControle.java
@@ -1,8 +1,13 @@
 package controles;
 
 import java.util.Collection;
+import java.util.LinkedList;
 
 import dao.PedidoDao;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 import modelos.Pedido;
 import modelos.Produto;
 
@@ -12,7 +17,23 @@ public class PedidoControle {
         pedidoDao.salvarPedido(pedido, produtos);
     }
 
-    public static void removerPedido() {
+    public static void removerPedido(String codigo) {
+        PedidoDao pedidoDao = new PedidoDao();
 
+        Optional<Pedido> optPedido = pedidoDao.select(Integer.parseInt(codigo));
+
+        if(optPedido.isPresent()) {
+            pedidoDao.excluirPedido(optPedido.get());
+        }else {
+            System.err.println("Código de pedido inválido");
+        }
     }
+    
+    public static Collection<Map<String, String>> listarPedidos() {
+        PedidoDao pedidoDao = new PedidoDao();
+        return pedidoDao.selectAll().stream()
+                                    .map(Pedido::toMap)
+                                    .collect(Collectors.toCollection(LinkedList::new));
+    }
+
 }
