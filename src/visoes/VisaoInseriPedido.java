ec1aa9b793521450f12d74630358730e38258d32
Mateus <mateuscirinosilva32@gmail.com>
2020-04-10 23:11:32 -0300

    teste

---
+++ src/visoes/VisaoInseriPedido.java
@@ -0,0 +1,42 @@
+package visoes;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.Optional;
+
+import controles.PedidoControle;
+import dao.ClienteDao;
+import dao.ProdutoDao;
+import dao.VendedorDao;
+import modelos.Cliente;
+import modelos.Pedido;
+import modelos.Produto;
+import modelos.Vendedor;
+
+public class VisaoInseriPedido {
+    public static void main(String[] args) {
+        ClienteDao clienteDao = new ClienteDao();
+        VendedorDao vendedorDao = new VendedorDao();
+
+        Optional<Cliente> optCliente = clienteDao.select(2);
+        Optional<Vendedor> optVendedor = vendedorDao.select(1);
+
+        if (optCliente.isPresent() && optVendedor.isPresent()) {
+            Cliente cliente = optCliente.get();
+
+            Vendedor vendedor = optVendedor.get();
+
+            Pedido pedido = new Pedido();
+            pedido.setObservacao("Meu primeiro teste simulando uma view");
+            pedido.setData(new Date());
+            pedido.setIdCliente(cliente.getId());
+            pedido.setIdVendedor(vendedor.getId());
+
+            ProdutoDao produtoDao = new ProdutoDao();
+            Collection<Produto> produtos = produtoDao.selectAll();
+            produtos.forEach(p -> p.setQuantidade(1.0));
+
+            PedidoControle.salvarPedido(pedido, produtos);
+        }
+    }
+}
